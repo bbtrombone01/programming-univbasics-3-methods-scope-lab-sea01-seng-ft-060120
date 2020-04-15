8f18eaedc5a9458f6a570902a00c875e7ecbebc3
steven <bbtrombone@sbcglobal.net>
2020-04-15 20:14:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,6 @@ def mario
 end
 
 def toadstool
-  status = "Thank you Mario! But Our Princess Is In Anthoer Castle!"
+  status = "Thank You Mario! But Our Princess Is In Anthoer Castle!"
   puts status
 end
